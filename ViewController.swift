2533223853c3be2a51bcf3aba5976befb7ca667a
mohfarahh <141188852+mohfarahh@users.noreply.github.com>
2023-09-29 21:19:26 -0500

    Add files via upload
    Files for Unit 2 project

--- ViewController.swift
+++ ViewController.swift
@@ -7,42 +7,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,
+                      SettingsViewControllerDelegate {
+  
   @IBOutlet weak var wordsCollectionView: UICollectionView!
   @IBOutlet weak var keyboardCollectionView: UICollectionView!
-
+  
   private var boardController: BoardController!
   private var keyboardController: KeyboardController!
-
+  
+  private let segueIdentifier = "SettingsViewControllerSegue"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     setupNavigationBar()
-
+    
     boardController = BoardController(collectionView: wordsCollectionView)
     keyboardController = KeyboardController(collectionView: keyboardCollectionView)
-    /*
-      Exercise 3: Assign a closure to the `didSelectString` property of `keyboardController` (see KeyboardController.swift):
-      
-      This closure takes in a string (the string selected from the keyboard).
-      If the string is equal to the `DELETE_KEY` constant (see Constants.swift), then call the `deleteLastCharacter` method of `boardController`.
-      Else, it should use the `enter` method of `boardController` and pass in the selected string as the argument.
-     */
-    // START YOUR CODE HERE
-    // ...
-      keyboardController.didSelectString = { [weak self] selectedString in
-          // Check if the selected string is equal to the DELETE_KEY constant.
-          if selectedString == DELETE_KEY {
-              // If it's DELETE_KEY, call the deleteLastCharacter method of boardController.
-              self?.boardController.deleteLastCharacter()
-          } else {
-              // If it's not DELETE_KEY, call the enter method of boardController and pass in the selected string.
-              self?.boardController.enter(selectedString)
-          }
+    keyboardController.didSelectString = { [unowned self] string in
+      if string == kDeleteKey {
+        self.boardController.deleteLastCharacter()
+      } else {
+        self.boardController.enter(string)
       }
-        
-      
+    }
+    let rightBarButtonItem = UIBarButtonItem(title: "Settings",
+                                             style: .plain,
+                                             target: self,
+                                             action: #selector(didTapSettingsButton))
+    rightBarButtonItem.tintColor = .white
+    navigationItem.rightBarButtonItem = rightBarButtonItem
+    // Exercise 5 Pt. 1 (optional): Add a button on the left-hand side of the navigation bar to reset the
+    // game with the current settings
+    // Tip 1: Look at how the `rightBarButton` is created and use it as an example. You may create a new
+    // function that gets called similar to `didTapSettingsButton`. Make use of the online documentation and
+    // CMD + click to learn more about what methods and properties you can use
+    // Tip 2: You'll want to use and implement `resetBoardWithCurrentSettings` inside of BoardController.swift
+    // in the function that you fire when the button is tapped
+    // START YOUR CODE HERE
+      let resetBarButton = UIBarButtonItem(title: "Reset",
+                                               style: .plain,
+                                               target: self,
+                                               action: #selector(didResetButton))
+      resetBarButton.tintColor = .white
+      navigationItem.leftBarButtonItem = resetBarButton
+    // ...
     // END YOUR CODE HERE
   }
+                    
+  
+  @objc private func didTapSettingsButton() {
+    performSegue(withIdentifier: segueIdentifier, sender: nil)
+  }
+  
+    @objc private func didResetButton() {
+        //BoardController.resetBoardWithCurrentSettings()
+       boardController.resetBoardWithCurrentSettings()
+    }
+    
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    guard segue.identifier == segueIdentifier else { return }
+    let settingsViewController = segue.destination as! SettingsViewController
+    settingsViewController.delegate = self
+  }
+  
+  func didChangeSettings(with settings: [String: Any]) {
+    boardController.resetBoard(with: settings)
+  }
 }
+
